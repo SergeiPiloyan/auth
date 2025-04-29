ca2960944947e71420ea5c66064fb4b5c67056ac
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2025-04-30 00:11:33 +0400

    [SERVER]: add handle auth controller

--- server/src/controllers.ts
+++ server/src/controllers.ts
@@ -2,6 +2,42 @@ import { Request, Response } from "express";
 
 export class Controllers {
   public static async handleAuth(req: Request, res: Response) {
-    res.send("handle auth is working");
+    const { user_name, password }: { user_name: string; password: string } =
+      req.body;
+
+    if (!user_name || !password) {
+      res
+        .status(400)
+        .json({ errorCode: 400, error_message: "Wrong credentials" });
+    }
+
+    const trimmedUserName = user_name?.trim();
+    const trimmedPassword = password?.trim();
+  }
+
+  private static sendJSON(
+    res: Response,
+    json: any = undefined,
+    cacheAge: number = 0
+  ) {
+    res.setHeader("Cache-Control", `public, max-age=${cacheAge}`);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+      "Access-Control-Expose-Headers",
+      `Content-Range,Content-Type`
+    );
+
+    res.json({ errorCode: 0, data: json });
+
+    res.end();
   }
 }
