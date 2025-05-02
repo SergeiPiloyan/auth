ec1a52b0be7bcba31888cb5aec1b425ad393c4fe
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2025-05-02 16:52:50 +0400

    [CLIENT]: add api

---
+++ client/src/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import toast from 'react-hot-toast';
+import { COOKIE_TOKEN, TECH_ERROR } from './utils/const';
+
+const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL });
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        if (response.data.errorCode !== 0) {
+            toast.error(response.data.message || TECH_ERROR);
+        }
+
+        return response;
+    },
+    (error) => toast.error(error.response?.data?.message || TECH_ERROR)
+);
+
+axiosInstance.interceptors.request.use((req) => {
+    const token = Cookies.get(COOKIE_TOKEN);
+    req.headers.set('Authorization', token);
+
+    return req;
+});
+
+export const apiPostReq = async (resource: string, body = {}) => {
+    const rs = await axiosInstance.post(resource, body);
+
+    return rs.data?.data;
+};
---
+++ client/src/utils/const.ts
@@ -0,0 +1,4 @@
+export const TECH_ERROR =
+    'Something went wrong. We are working on getting this fixed as soon as possible.';
+
+export const COOKIE_TOKEN = 'auth_cookie_token';
