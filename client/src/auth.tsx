124b6ae31fab04f5b4b8dc9350989ccb6f74bdc3
Sergei_Piloyan <sergei.piloyan0302@gmail.com>
2025-05-02 16:53:29 +0400

    [CLIENT]: add auth context

---
+++ client/src/auth.tsx
@@ -0,0 +1,75 @@
+import { createContext, RefObject, useContext, useRef, useState } from 'react';
+import { COOKIE_TOKEN } from './utils/const';
+import { useNavigate } from 'react-router-dom';
+import { apiPostReq } from './api';
+import Cookies from 'js-cookie';
+
+type TAuthContext = {
+    isAuth: boolean;
+    isAuthChecked: RefObject<boolean | null>;
+    checkAuth: (forceCheck?: boolean) => Promise<void>;
+    login: (data: { user_name: string; password: string }) => Promise<void>;
+    logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<TAuthContext>({} as TAuthContext);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [isAuth, setIsAuth] = useState(false);
+    const isAuthChecked = useRef(null);
+    const navigate = useNavigate();
+
+    const checkAuth = async (forceCheck?: boolean) => {
+        if (!isAuthChecked.current || forceCheck) {
+            const token = Cookies.get(COOKIE_TOKEN);
+
+            if (token) {
+                setIsAuth(true);
+                if (window.location.pathname === '/login') {
+                    navigate('/');
+                }
+                navigate(window.location.pathname);
+                return;
+            }
+            await logout();
+        } else {
+            console.log('Already checked');
+        }
+    };
+
+    const login = async (values: { user_name: string; password: string }) => {
+        const data = await apiPostReq('auth', values);
+
+        if (data.cookieToken) {
+            Cookies.set(COOKIE_TOKEN, data.cookieToken, { expires: 9999 });
+            await checkAuth(true);
+        }
+    };
+
+    const logout = async () => {
+        setIsAuth(false);
+        navigate('/login');
+        const token = Cookies.get(COOKIE_TOKEN);
+        if (token) {
+            await apiPostReq('logout', { token });
+        }
+
+        Cookies.remove(COOKIE_TOKEN);
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isAuth,
+                isAuthChecked,
+                checkAuth,
+                login,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
